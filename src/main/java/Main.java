aae5bd1b9305a9976633ee5e16533e136d4d2177
Larisa Gurina <larisa.gurina.s@gmail.com>
2021-01-29 03:06:14 +0300

    Statistica errer sumcalculate

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        //подготавливаем данные
+        String[] monthsnames = new String[]{"JAN", "FEB", "MRCH", "APRL", "MAY", "JUN", "JUL", "AUG", "SPT", "OCT", "NOV", "DEC"};
+        int[] gains = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        for (int gain : gains) {
+            System.out.println(gain);
+        }
+        int[] gains = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        public long calculateSum(long[] gains) {
+            long sum = 0;
+            for (long gain : gains){
+                sum += gain;
+            }
+            return sum;
+        }
+    }
+}
